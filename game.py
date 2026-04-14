fb48472eebf953ebbc722eb67e09e35fb4723985
Valdemar Kotenko <vkotenki@gmail.com>
2026-04-14 09:15:27 +0500

    Upd1

--- game.py
+++ game.py
@@ -8,7 +8,7 @@ from gameparts.exceptions import CellOccupiedError, FieldIndexError
 def save_result(result):
     # Открыть файл results.txt в режиме "добавление".
     # Если нужно явно указать кодировку, добавьте параметр encoding='utf-8'.
-    file = open('results.txt', 'a')
+    file = open('results.txt', 'a', encoding='utf-8') 
     # Записать в файл содержимое переменной result.
     file.write(result + '\n')
     file.close()
